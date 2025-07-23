74ac34caa069fdc06c94f07b24383604950956a0
pawelpalicki <pawelpalicki@gmail.com>
2025-07-23 08:15:29 +0000

    fix: Implementacja strategii czyszczenia cache w Service Workerze

--- APP/sw.js
+++ APP/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tracker-wydatkow-cache-v2';
+const CACHE_NAME = 'tracker-wydatkow-cache-v4'; // Zwiększona wersja
 const urlsToCache = [
     '/',
     '/tracker.html',
@@ -6,24 +6,41 @@ const urlsToCache = [
     '/manifest.json'
 ];
 
+// Instalacja Service Workera i cache'owanie zasobów
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Otwarto cache:', CACHE_NAME);
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+// Aktywacja Service Workera i czyszczenie starych cache'y
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        console.log('Usuwanie starego cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Przechwytywanie zapytań sieciowych
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        // Najpierw spróbuj pobrać z sieci, aby zawsze mieć najnowszą wersję
+        fetch(event.request).catch(() => {
+            // Jeśli sieć zawiedzie, spróbuj pobrać z cache
+            return caches.match(event.request);
+        })
+    );
 });
